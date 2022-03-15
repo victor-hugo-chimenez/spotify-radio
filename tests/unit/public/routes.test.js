70dad4714e847f7ff39bbd14dc2bf55fcce9ad88
Victor Hugo Chimenez <victor.hugo@gringo.com.vc>
2022-03-15 01:53:17 -0300

    feat: aula 1

---
+++ tests/unit/public/routes.test.js
@@ -0,0 +1,12 @@
+import {
+    jest,
+    expect,
+    describe,
+    test
+} from '@jest/globals'
+
+describe('#Routes', () => {
+    test('my test', () => {
+        expect(true).toBeTruthy()
+    })
+})
